e1a998baf597a0a58a356e057deea4c57af5cdca
uerceg <ugi@adjust.com>
2019-03-05 13:58:29 +0100

    Adjust path to ext/scripts

--- adobe/utils.py
+++ adobe/utils.py
@@ -102,10 +102,8 @@ def get_scripts_dir():
 # Get SDK root directory path.
 def get_root_dir():
     dir_scripts = get_scripts_dir()
-    # After we add scripts as a submodule to /ext/scripts:
-    # dir_ext = path_navigate_down(dir_scripts)
-    # return path_navigate_down(dir_ext)
-    return path_navigate_down(dir_scripts)
+    dir_ext = path_navigate_down(dir_scripts)
+    return path_navigate_down(dir_ext)
 
 # Get partent directory of the given path.
 def path_navigate_down(dir_path):
