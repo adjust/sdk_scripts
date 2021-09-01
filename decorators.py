7a5a0d53f2040727c3820019ac67782c33cb36ef
Maxim Kuznetsov <kyzya96@gmail.com>
2021-09-01 08:04:30 +0300

    fixing bug with iOS system name

--- decorators.py
+++ decorators.py
@@ -4,7 +4,7 @@ import platform
 def only_mac_os(func):
     """Decorator for macOS only functions."""
     def wrapper(*args, **kwargs):
-        if platform.system() == 'Darvin':
+        if platform.system() == 'Darwin':
             func(*args, **kwargs)
         else:
             print(f'[{func.__name__}] This is not working on {platform.system()} only for Mac!')
